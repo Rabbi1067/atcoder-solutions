bf9a2dc5e3ff2945dd32c29b619a2cb73b58b00b
Fazle Rabbi <fazlerabbicse65@gmail.com>
2025-03-15 18:33:53 +0600

    Create A - Thermometer.cpp

---
+++ A - Thermometer.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+float n;
+cin>>n;
+if(n>=38) cout<<1<<endl;
+else if(38>=n && 37.5<=n) cout<<2<<endl;
+else cout <<3<<endl;
+return 0;
+}
