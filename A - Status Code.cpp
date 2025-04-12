e4c417a930814c567ee8a32f233ff4aae5fdc06e
Fazle Rabbi <fazlerabbicse65@gmail.com>
2025-04-12 18:08:20 +0600

    Create A - Status Code.cpp

---
+++ A - Status Code.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(false); 
+    cin.tie(0);
+ int n;
+ cin>>n;
+ if(200<=n && n<=299) cout<<"Success"<<endl;
+ else cout<<"Failure"<<endl;
+ 
+    return 0;
+}
