cc987955c8d922e6f2850cf75399a65f524e3dae
Fazle Rabbi <fazlerabbicse65@gmail.com>
2025-05-17 18:59:11 +0600

    Create A - Not Acceptable.cpp

---
+++ A - Not Acceptable.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int a,b,c,d;
+  cin>>a>>b>>c>>d;
+  int x=a*60+b;
+    int y=c*60+d;
+  if(x>y) cout<<"YES"<<endl;
+  else cout<<"NO"<<endl;
+  return 0;
+}
